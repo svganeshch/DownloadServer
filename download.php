03ec32763d87ee089300ef7cc88d80e95a3a0387
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-09-09 20:48:43 +0530

    download: Fix download url

--- download.php
+++ download.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $insertedData = insertFileUrl($file->file_sha256);
                 $token_identifier = $insertedData['token_identifier'];
             }
-            exit(SERVER_DOWN_URL."download.php?token={$token_identifier}?version={$file_version}?variant={$file_variant}");
+            exit(SERVER_DOWN_URL."download.php?token={$token_identifier}&version={$file_version}&variant={$file_variant}");
         } else {
             exit("No file exists or no longer available");
         }
